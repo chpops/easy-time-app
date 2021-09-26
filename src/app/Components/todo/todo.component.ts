766310a1bd23772bc5b5358835ecbd561d8cea88
Choper <nikichtube@gmail.com>
2021-09-26 04:01:30 +0300

    fixed ToDo-Component with cdkDropList and add new .gitignore file

--- src/app/Components/todo/todo.component.ts
+++ src/app/Components/todo/todo.component.ts
@@ -3,49 +3,55 @@ import { AuthService } from '../../shared/services/auth.service';
 import { Subscription } from 'rxjs';
 import { ListType, TodoItem } from 'src/app/shared/interfaces';
 import { Router } from '@angular/router';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-
 export class TodoComponent implements OnInit {
-  aSub: Subscription = new Subscription()
-  todos: TodoItem[] = []
+  todo = [];
+  process = [];
+  done = [];
+
+  aSub: Subscription = new Subscription();
+  todos: TodoItem[] = [];
   listTypeEnum = ListType;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-  ) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    if(this.authService.loggedIn()){
-      console.log('[TodoComponent] - You are already logged in! Your token = ' + localStorage.getItem('token'));
-      this.authService.getTodosList().subscribe(({ todos }) => {
-        this.todos = todos;
-      })
-    }
-    else{
-      console.log('[TodoComponent] - You are NOT logged! Please login!');
-      this.router.navigate(['/forbidden']);
-    }
+    return this.authService.getTodosList().subscribe(({ todos }) => {
+      this.todos = todos;
+      this.todo = this.getList(ListType.ToDo);
+      this.process = this.getList(ListType.InProcess);
+      this.done = this.getList(ListType.Done);
+    });
   }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-    };
-  };
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
   public getList(type: ListType): TodoItem[] {
-    return this.todos.filter(item => item.list === type);
+    return this.todos.filter((item) => item.list === type);
   }
 }
